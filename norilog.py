5eed02c0fba8388d4b54f43c82dfdec689b66080
raimon <raimon49@hotmail.com>
2019-04-04 00:03:50 +0900

    Implement routing with Flask

--- norilog.py
+++ norilog.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
 import json
 
+from flask import Flask, render_template
+
+application = Flask(__name__)
+
 DATA_FILE = 'norilog.json'
+
 
 def save_data(start, finish, memo, created_at):
     """記録データを保存します
@@ -38,3 +44,15 @@ def load_data():
     except FileNotFoundError:
         database = []
     return database
+
+
+@application.route('/')
+def index():
+    """トップページ
+    テンプレートを使用してページを表示します
+    """
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    application.run('0,0.0.0', 8000, debug=True)
