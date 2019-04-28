e486b427c2370ad908becb31893daf57a8744cb5
raimon <raimon49@hotmail.com>
2019-04-28 14:53:35 +0900

    Update metadata and classifiers

--- setup.py
+++ setup.py
@@ -1,11 +1,36 @@
+import os
 from setuptools import setup, find_packages
 
 
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname(__file__))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
+
+
 setup(
-    name='norilog',
+    name='raimon49.norilog',
     version='1.0.0',
+    description='The NoriLog web application.',
+    long_description=read_file('README.rst'),
+    author='raimon49',
+    author_email='raimon49@hotmail.com',
+    url='https://github.com/raimon49/pypro3-ch02-ch03',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Flask',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires=">=3.6",
     packages=find_packages(),
     include_package_data=True,
+    keywords=['web', 'norilog'],
+    license='BSD License',
     install_requires=[
         'Flask',
     ],
